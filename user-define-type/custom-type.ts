6aa7564e8f7c8a24e92ad10ae180ca5ad829c29c
IshratMishu <ishratjahan0425@gmail.com>
2025-01-01 22:58:58 +0600

    new year

---
+++ user-define-type/custom-type.ts
@@ -0,0 +1,15 @@
+
+let user1: object;
+user1 = {name: 'mISHU' , id: 11};
+
+let user2: object[];
+user2 = [{name: 'mISHU' , id: 11}, {name: 'mHU' , id: 11}];
+
+let users: object[];
+users = [];
+
+let user3: { userName: string , userId: number};
+user3 = { userName: 'Mishu', userId: 11 };
+users.push(user3);
+
+console.log(users);
